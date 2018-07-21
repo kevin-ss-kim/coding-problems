3d0a68b8f63ce07591ca6eee771ecd99d1dee929
kevin kim <sskevinkim@gmail.com>
2018-07-20 23:55:43 -0700

    changed k frequent elements to use buckets

--- array/find_k_frequent_elements.py
+++ array/find_k_frequent_elements.py
@@ -1,42 +1,28 @@
 def find_top_k_frequent_element(nums, k):
+    a = [None] * (len(nums) + 1)
     elementToIndex = dict()
-    indexToElement = dict()
-    currentIndex = 0
     for value in nums:
         if value in elementToIndex:
-            print(str(value) + " already found")
-            nums[elementToIndex[value]] += 1
-            # Bubble up and update index
-            bubbleUpIndex = elementToIndex[value]
-            parentIndex = (bubbleUpIndex - 1) / 2
-            while (parentIndex >= 0) and (nums[bubbleUpIndex] > nums[parentIndex]):
-                print("bubbling up index from " + str(bubbleUpIndex) + " to parent index " + str(parentIndex))
-                # Bubble up and set dictionary
-                childElement = indexToElement[bubbleUpIndex]
-                parentElement = indexToElement[parentIndex]
-                indexToElement[bubbleUpIndex] = parentElement
-                indexToElement[parentIndex] = childElement
-                elementToIndex[childElement] = nums[parentIndex]
-                elementToIndex[parentElement] = nums[bubbleUpIndex]
-                bubbleUpIndex = parentIndex
-                parentIndex = (parentIndex - 1) / 2
+            index = elementToIndex[value]
+            a[index].remove(value)
+            if a[index + 1] is None:
+                a[index + 1] = [value]
+            else:
+                a[index + 1] = a[index + 1].append(value)
+            elementToIndex[value] = index + 1
         else:
-            print(str(value) + " not found, inserting")
-            nums[currentIndex] = 1
-            indexToElement[currentIndex] = value
-            elementToIndex[value] = currentIndex
-            currentIndex += 1
-        print(nums[:currentIndex])
-        print(indexToElement)
-    a = []
-    while k > 0:
-        # remove element, then bubble down
-        a.append(indexToElement[0])
-        nums[0] = nums[currentIndex]
-        currentIndex -= 1
-        topIndex = 0
-        checkIndex = nums[1] > num[2] then 1 else 2
-    return a
+            if a[1] is None:
+                a[1] = [value]
+            else:
+                a[1] = a[1].append(value)
+            elementToIndex[value] = 1
+    returnList = []
+    currIndex = len(nums)
+    while k >= 0:
+        if a[currIndex] != None and a[currIndex] != []:
+            returnList.append(a[currIndex].pop())
+            k -= 1
+    return returnList
 
 def main():
     array = input("Enter sorted array: ")
