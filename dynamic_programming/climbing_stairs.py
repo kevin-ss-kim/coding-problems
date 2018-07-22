ac40e747b94e4691e689e3d87d813d837f039d01
kevin kim <sskevinkim@gmail.com>
2018-07-21 22:27:07 -0700

    added climb stairs with fibonacci

--- dynamic_programming/climbing_stairs.py
+++ dynamic_programming/climbing_stairs.py
@@ -3,8 +3,8 @@ def climbStairs(n):
     :type n: int
     :rtype: int
     """
-    array = [None] * (n + 1)
-    return climbStairsWithMemo(n, array)
+    return climbStairsWithFibonacci(n)
+
 def climbStairsWithMemo(n, array):
     if n < 0:
         return 0
@@ -15,6 +15,16 @@ def climbStairsWithMemo(n, array):
     else:
         array[n] = climbStairsWithMemo(n - 1, array) + climbStairsWithMemo(n - 2, array)
         return array[n]
+
+def climbStairsWithFibonacci(n):
+    if n == 1:
+        return 1
+    first, second = 1, 2
+    for i in xrange(3, n + 1):
+        third = first + second
+        first = second
+        second = third
+    return third
 
 if __name__ == '__main__':
     n = input("Enter number of steps: ")
