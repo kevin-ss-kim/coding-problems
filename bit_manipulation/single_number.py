a27b1b53859f48aa352f62d717971598717d32e5
kevin kim <sskevinkim@gmail.com>
2018-07-25 23:29:14 -0700

    clean up

--- bit_manipulation/single_number.py
+++ bit_manipulation/single_number.py
@@ -3,16 +3,15 @@ def singleNumber(nums):
     :type nums: List[int]
     :rtype: int
     """
-    ones = 0;  
-    twos = 0;  
+    ones = 0
+    twos = 0
 
     for i in xrange(len(nums)):   
         x = nums[i]
-        twos |= ones & x;  
-        ones ^= x;  
-        not_threes = ~(ones & twos);  
-        ones &= not_threes;  
-        twos &= not_threes;  
+        twos |= ones & x
+        ones ^= x
+        not_threes = ~(ones & twos)
+        ones, twos = ones & not_threes, twos & not_threes
     return ones
 
 if __name__ == '__main__':
