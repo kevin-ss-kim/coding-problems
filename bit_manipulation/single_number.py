96d61c83eaa14412ea0ad11a3f7752cf91dc1697
kevin kim <sskevinkim@gmail.com>
2018-07-25 23:24:30 -0700

    added find single number

---
+++ bit_manipulation/single_number.py
@@ -0,0 +1,20 @@
+def singleNumber(nums):
+    """
+    :type nums: List[int]
+    :rtype: int
+    """
+    ones = 0;  
+    twos = 0;  
+
+    for i in xrange(len(nums)):   
+        x = nums[i]
+        twos |= ones & x;  
+        ones ^= x;  
+        not_threes = ~(ones & twos);  
+        ones &= not_threes;  
+        twos &= not_threes;  
+    return ones
+
+if __name__ == '__main__':
+    array = input("Enter array: ")
+    print(singleNumber(array))
