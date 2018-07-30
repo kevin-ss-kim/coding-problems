73173e974a25f09c0b535d15e0733a26d7cf4bb7
kevin kim <sskevinkim@gmail.com>
2018-07-29 23:28:33 -0700

    added specs for serialize and deserialize binary tree

---
+++ tree/serialize_deserialize_binary_tree.py
@@ -0,0 +1,22 @@
+class Node:
+    def __init__(self, val, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+class Codec:
+
+    def serialize(self, root):
+        """Encodes a tree to a single string.
+        
+        :type root: TreeNode
+        :rtype: str
+        """
+        
+
+    def deserialize(self, data):
+        """Decodes your encoded data to tree.
+        
+        :type data: str
+        :rtype: TreeNode
+        """
