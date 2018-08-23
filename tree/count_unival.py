cf439186d6f2722f433003bdc2f2bde36330b387
kevin kim <sskevinkim@gmail.com>
2018-08-22 23:59:23 -0700

    edit implementation

--- tree/count_unival.py
+++ tree/count_unival.py
@@ -16,22 +16,22 @@ For example, the following tree has 5 unival subtrees:
 
 
 def count_unival(tree):
-    return count_unival_length(0, tree, None)
+    return count_unival_length(0, tree, -1)
 
-# For any given node, the unival count is
-# 1 if the node value is equal to left or right child
-# 3 if the node value is equal to left and right child
-def count_unival_length(pos, tree, parent_value):
+def count_unival_length(pos, tree, chain_pos):
+    print("calling count_unival_length (%s, %s)" % (str(pos), str(chain_pos)))
     if pos >= len(tree):
         return 0
-    match_parent, extra_value = 0, 0
-    if tree[pos] == parent_value:
+    match_parent, extra_value, new_chain_pos = 0, 0, pos
+    if tree[pos] == tree[chain_pos]:
         match_parent = 1
-    left_unival_count = count_unival_length(2 * pos + 1, tree, tree[pos])
-    right_unival_count = count_unival_length(2 * pos + 2, tree, tree[pos])
-    if left_unival_count and right_unival_count:
+    # Check if the pos is at least left grandchild of chain_pos
+    if chain_pos >= 0 and chain_pos * 4 + 3 <= pos and tree[chain_pos] == tree[pos]:
         extra_value = 1
-    return left_unival_count + right_unival_count + match_parent + extra_value
+        new_chain_pos = chain_pos
+    left_unival_count = count_unival_length(2 * pos + 1, tree, new_chain_pos)
+    right_unival_count = count_unival_length(2 * pos + 2, tree, new_chain_pos)
+    return left_unival_count + right_unival_count + extra_value + match_parent
     
 # Define binary tree as array
 # Given a node at index n, the left and right child
