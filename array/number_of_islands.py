a30a366976fc6a175bab5fe6c7174330c6ee522b
Kevin Kim <sskevinkim@gmail.com>
2018-12-12 22:22:30 -0800

    add bfs for number of islands

--- array/number_of_islands.py
+++ array/number_of_islands.py
@@ -28,11 +28,24 @@ def dfs_explore(grid, x, y):
         dfs_explore(grid, x + 1, y)
         dfs_explore(grid, x, y + 1)
 
+def bfs_explore(grid, x, y):
+    queue = [(x, y)]
+    while len(queue) > 0:
+        island = queue.pop()
+        grid[island[0]][island[1]] = 0
+        if island[0] + 1 < len(grid):
+            if grid[island[0] + 1][island[1]] == 1:
+                queue.append((island[0] + 1, island[1]))
+        if island[1] + 1 < len(grid[0]):
+            if grid[island[0]][island[1] + 1] == 1:
+                queue.append((island[0], island[1] + 1))
+
 def num_islands(grid):
     num_islands = 0
     for x in range(len(grid)):
         for y in range(len(grid[x])):
             if grid[x][y] == 1:
                 num_islands += 1
-                dfs_explore(grid, x, y)
+                #dfs_explore(grid, x, y)
+                bfs_explore(grid, x, y)
     return num_islands
