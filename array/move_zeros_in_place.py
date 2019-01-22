5498af586618e1f284cfb460994fadab76f26914
Kevin Kim <sskevinkim@gmail.com>
2019-01-21 23:52:45 -0800

    add move zeroes in-place

---
+++ array/move_zeros_in_place.py
@@ -0,0 +1,20 @@
+'''
+Given an array nums, write a function to move all 0's to the end of it while maintaining the relative order of the non-zero elements.
+
+Example:
+
+Input: [0,1,0,3,12]
+Output: [1,3,12,0,0]
+Note:
+
+You must do this in-place without making a copy of the array.
+Minimize the total number of operations.
+'''
+
+def moveZeroes(nums):
+    i, j = 0, 0
+    while i < len(nums):
+        if nums[i] != 0:
+            nums[i], nums[j] = nums[j], nums[i]
+            j += 1
+        i += 1
